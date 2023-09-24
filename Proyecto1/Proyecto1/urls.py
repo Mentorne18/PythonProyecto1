f42efb0e3a938e4a89ae7448b933c5328889bad1
Danilo Rajcevic <danilorajcevic14@gmail.com>
2023-09-24 20:58:53 -0300

    Creé el archivo views.py con el request saludo y lo agregué al archivo urls.py

--- Proyecto1/Proyecto1/urls.py
+++ Proyecto1/Proyecto1/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from Proyecto1.views import saludo #para acceder a la vista hay que importar el modulo(Proyecto1) y el metodo(views.py)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("bienvenida/", saludo),
 ]
